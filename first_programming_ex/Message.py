511ab8cbd4675c2ff15cbca76a81527ed745ab57
S1044345 <144111345+S1044345@users.noreply.github.com>
2023-10-18 21:04:24 +0200

    Made some changes on Message.py

--- first_programming_ex/Message.py
+++ first_programming_ex/Message.py
@@ -11,7 +11,24 @@ class Message:
     def __init__ (self, sender, recipient):
         self.sender = sender
         self.recipient = recipient
+        self.body = ""
 
+    #Method that adds a line to the body of the email
+    def append (self, line):
+        self.body += line + "\n"
+        self.log_messages()
+
+    #Method that converts the message into a string
+    def toString (self):
+        message = "From: {}\nTo: {}\n\n{}\n".format(self.sender,self.recipient,self.body)
+        return message
+
+    #Method that automatically increases no of messages and add a log of the message
+    def log_messages (self):
         Message._no_messages = Message._no_messages + 1
+        if self.sender not in Message._log:
+            Message._log[self.sender] = {}
 
-     
+        Message._log[self.sender][self.recipient] = self.body
+        
+        return Message._no_messages,Message._log
